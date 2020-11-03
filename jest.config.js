e12903f0fdeb76d1c060db3670fb4797e721658a
WeslleyFratini <weeslleey354@gmail.com>
2020-11-02 21:19:34 -0300

    load user by email respository

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   coverageDirectory: "coverage",
   testEnvironment: "node",
   collectCoverageFrom: ["**/src/**/*.js"],
+  preset: "@shelf/jest-mongodb",
 };
---
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -0,0 +1,72 @@
+const { MongoClient } = require("mongodb");
+
+let client, db;
+
+class LoadUserByEmailRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
+
+  async load(email) {
+    const user = await this.userModel.findOne(
+      {
+        email,
+      },
+      {
+        projection: {
+          password: 1,
+        },
+      }
+    );
+    return user;
+  }
+}
+
+const makeSut = () => {
+  const userModel = db.collection("users");
+  const sut = new LoadUserByEmailRepository(userModel);
+  return {
+    userModel,
+    sut,
+  };
+};
+
+describe("LoadUserByEmail Repository", () => {
+  beforeAll(async () => {
+    client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = client.db();
+  });
+
+  beforeEach(async () => {
+    await db.collection("users").deleteMany();
+  });
+
+  afterAll(async () => {
+    await client.close();
+  });
+
+  test("Should return null if no user is found", async () => {
+    const { sut } = makeSut();
+    const user = await sut.load("invalid_email@mail.com");
+    expect(user).toBeNull();
+  });
+
+  test("Should return an user if user is found", async () => {
+    const { sut, userModel } = makeSut();
+    const fakeUser = (await userModel = insertOne({
+      email: "valid_email@mail.com",
+      name: "any_name",
+      age: 50,
+      state: "any_state",
+      password: "hashed_password",
+    }));
+    const user = await sut.load("valid_email@mail.com");
+    expect(user).toEqual({
+      _id: fakeUser.ops[0]._id,
+      password: fakeUser.ops[0].password,
+    });
+  });
+});
